f9fe0eadb525ae6adddf15cea0156484c4bedf20
anushanga pavith <anushangapavith2@gmail.com>
2022-07-02 13:55:32 +0530

    /Data file can now show data in UI

--- application/src/pages/Data.js
+++ application/src/pages/Data.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 function Data() {
+
+    const [posts, setPosts] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:3001/api/machines')
         // fetch('/api/machines')   // This line suppose to work with setupProxy.js file. but it doesn't. So the above line works fine
-        .then(results => results.json())
-        .then(jsonData => console.log(jsonData))
+        .then(results => {
+            return results.json()
+        })
+        .then(jsonData => {
+            setPosts(jsonData) 
+        });
+
     }, []);
 
     return (
         <div className='machine-data-container'>
             <h2>Data from database</h2>
+            <ul>
+                {
+                    posts.map(post => <li key={post.machineID}>{post.machineID}</li>)
+                }
+            </ul>
         </div>
     )
 
