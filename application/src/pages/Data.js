18779480bf16760fa678714c5c72b5e79dc0800e
anushanga pavith <anushangapavith2@gmail.com>
2022-07-02 13:23:23 +0530

    Add module, setupProxy.js file, add data file

--- application/src/pages/Data.js
+++ application/src/pages/Data.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {useEffect} from 'react';
 
 function Data() {
-    const apiUrl = 'http://localhost:3001/api/machines'
-    
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/machines')
+        // fetch('/api/machines')   // This line suppose to work with setupProxy.js file. but it doesn't. So the above line works fine
+        .then(results => results.json())
+        .then(jsonData => console.log(jsonData))
+    }, []);
+
+    return (
+        <div className='machine-data-container'>
+            <h2>Data from database</h2>
+        </div>
+    )
+
 }
 
 export default Data;
