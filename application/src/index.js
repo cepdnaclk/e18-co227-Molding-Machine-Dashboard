c5b05acef0f5af5686c42450980c9b2e391c552f
Didula Induwara <didulainduwaraamarasinghe@gmail.com>
2022-07-09 00:38:31 +0530

    Add content to the New Machine page.

--- application/src/index.js
+++ application/src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './config/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   // </React.StrictMode>
 );
